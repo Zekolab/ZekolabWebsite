055af653f1b68ee9ac1a54d64535ef1a4c16460d
kilelx <llx.kieran@gmail.com>
2022-11-07 15:49:01 +0100

    Second last commit

--- scripts/smooth-scroll.js
+++
@@ -1,9 +0,0 @@
-let heightHeader = document.querySelector("header").offsetHeight;
-
-let links = document.querySelectorAll(".toc > a");
-
-links.forEach((link) => {
-    link.addEventListener("click", () => {
-        console.log("link");
-    })
-})
