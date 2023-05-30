58a63abe05596b07df75fbb0c864f95451c9964a
kieran <llx.kieran@gmail.com>
2023-05-30 21:21:54 +0200

    add: price changer

--- scripts/tarifs.js
+++ scripts/tarifs.js
@@ -1,14 +1,24 @@
 const pricings = document.querySelectorAll('.pricing_choice');
+const changingPrice = document.querySelector('.price_to_change')
 
 pricings.forEach(elt => {
-    console.log(elt);
     elt.addEventListener('click', () => {
         const currentlyActive = document.querySelector(".pricing_choice.active");
-        if (currentlyActive && currentlyActive!==elt) {
+        if (currentlyActive!==elt) {
             currentlyActive.classList.toggle("active");
             elt.classList.toggle("active");
-        } else {
-            elt.classList.toggle("active");
+            // console.log(elt.getAttribute('data-value'));
+            switch (elt.getAttribute('data-value')) {
+                case '1' :
+                    elt.innerText = '49,99';
+                    break;
+                case '6' :
+                    elt.innerText = '269,99';
+                    break;
+                case '1' :
+                    elt.innerText = '499,99';
+                    break;
+            }
         }
     })
 })
