243e7e2a4a2b8e5f994cc8aa648c80dde5083f01
kieran <llx.kieran@gmail.com>
2023-05-30 21:53:27 +0200

    update: card style

--- scripts/tarifs.js
+++ scripts/tarifs.js
@@ -26,12 +26,7 @@ pricings.forEach(elt => {
 /**
  * Smooth scroll
  */
-const lenis = new Lenis(
-//     {
-//     duration: 1.2,
-//     easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t))
-// }
-);
+const lenis = new Lenis();
 
 function raf(time) {
     lenis.raf(time);
