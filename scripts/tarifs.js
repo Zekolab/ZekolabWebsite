9e3b2cf3a383a847e65b80520af4ec83e50d8cf3
kieran <llx.kieran@gmail.com>
2023-06-02 07:40:52 +0200

    add: discount badge

--- scripts/tarifs.js
+++ scripts/tarifs.js
@@ -1,5 +1,7 @@
 const pricings = document.querySelectorAll('.pricing_choice');
-const changingPrice = document.querySelector('#price_to_change')
+const changingPrice = document.querySelector('#price_to_change');
+const discount = document.querySelector('.discount');
+const discountValue = document.querySelector('.discount-value');
 
 pricings.forEach(elt => {
     elt.addEventListener('click', () => {
@@ -10,12 +12,17 @@ pricings.forEach(elt => {
             switch (elt.getAttribute('data-value')) {
                 case '1' :
                     changingPrice.innerHTML = '<span class="euro">€ </span>49,99 <span class="month">/ 1 mois</span>';
+                    discount.style.display = 'none';
                     break;
                 case '6' :
                     changingPrice.innerHTML = '<span class="euro">€ </span>269,99 <span class="month">/ 6 mois</span>';
+                    discount.style.display = 'flex';
+                    discountValue.innerText = '-10%';
                     break;
                 case '12' :
                     changingPrice.innerHTML = '<span class="euro">€ </span>499,99 <span class="month">/ 12 mois</span>';
+                    discount.style.display = 'flex';
+                    discountValue.innerText = '-15%';
                     break;
             }
         }
@@ -31,15 +38,12 @@ function raf(time) {
     lenis.raf(time);
     ScrollTrigger.update();
     requestAnimationFrame(raf);
-    // console.log(section_promote.offsetWidth);
 }
 
 requestAnimationFrame(raf);
 
 const section_promote = document.querySelector('.pricing_promote');
 let box_items = gsap.utils.toArray(".promote_card");
-
-console.log(- 100 * box_items.length);
 
 gsap.to(box_items, {
   xPercent: -100 * (box_items.length - 0.75),
